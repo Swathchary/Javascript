9f20eff80a1b964e94e4270e317742767383b8d5
Swatchary <swathchary@gmail.com>
2026-04-17 22:13:06 +0530

    Form validation

--- Evenlistener.js
+++ Evenlistener.js
@@ -11,6 +11,58 @@ let sub_result = document.getElementById("display_sub_results");
 let Multi_result = document.getElementById("display_Multi_results");
 
 
+let group_list = document.getElementById("btn_group");
+
+
+
+group_list.addEventListener("click", function (e) {
+
+
+    console.log("nsmnsm", e.target);
+
+    if (e.target.textContent === "btn_sub") {
+
+        let first_num = document.getElementById("num1").value
+        let second_num = document.getElementById("num2").value
+
+
+        let total_sub = Number(first_num) - Number(second_num);
+
+        console.log("subbb", total_sub , e.target.textContent );
+        sub_result.innerText = "Subtraction : " + total_sub;
+
+
+    } else if (e.target.textContent === "btn_add") {
+
+        console.log("aftervalue", "enter here");
+        let first_num = document.getElementById("num1").value
+        let second_num = document.getElementById("num2").value
+
+        let total = Number(first_num) + Number(second_num);
+
+        add_result.innerText = "Addition : " + total;
+
+
+    } else if (e.target.textContent === "btn_Multi") {
+
+        let first_num = document.getElementById("num1").value
+        let second_num = document.getElementById("num2").value
+
+
+        let total_multi = Number(first_num) * Number(second_num);
+
+        Multi_result.innerText = "Muitiplication : " + total_multi;
+
+
+
+    }
+    console.log("hjdhfjs", e.target.textContent);
+
+
+})
+
+
+
 btnMultly.addEventListener("click", () => {
 
     let first_num = document.getElementById("num1").value
@@ -20,8 +72,6 @@ btnMultly.addEventListener("click", () => {
     let total_multi = Number(first_num) * Number(second_num);
 
     Multi_result.innerText = "Muitiplication : " + total_multi;
-
-
 
 });
 
